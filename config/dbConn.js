8609ebddaa8d7cc7c7d303cbc2961fdff3a9b8be
Rohit Pandey <rd8614196@gmail.com>
2024-04-19 21:15:29 +0530

    Changes made

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/JWT";
+const DB_URL = process.env.DATABASE_URI
+// || "mongodb://localhost:27017/JWT";
 const dbConn = () => {
   mongoose.connect(DB_URL, {
     useNewUrlParser: true,
